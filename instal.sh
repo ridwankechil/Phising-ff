2e76da54b707eb8dd8035f61be1152c46523bc6a
ridwankechil <70096802+ridwankechil@users.noreply.github.com>
2020-10-12 18:06:51 +0700

    Create instal.sh

---
+++ instal.sh
@@ -0,0 +1,14 @@
+#!/data/data/com.termux/files/home/bash
+apt update && apt upgrade -y
+clear
+pkg install python
+clear
+apt install curl 
+clear
+pkg install bash
+clear
+pip install requests
+clear
+pip install mechanize
+clear
+sh WRsh
